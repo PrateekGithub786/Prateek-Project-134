cb70e676ab62497ad304370da8b3d4ba2a5253ba
PrateekGithub786 <75919841+PrateekGithub786@users.noreply.github.com>
2021-09-02 20:47:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,60 @@
+object = [];
+status = "";
+
+function preload(){
+    sound = loadSound("astronaut.mp3");
+}
+
+function setup(){
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380, 380);
+    objectDetect = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Object";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded");
+    status = true;
+    objectDetect.detect(video, gotResults);
+}
+
+function gotResults(error, results){
+    if(error){
+        console.log(error)
+    }
+    console.log(results);
+    object = results;
+}
+
+function draw(){
+    image(video, 0, 0, 380, 380);
+    
+    if(status != ""){
+        for(i = 0; i < object.length; i++){
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+
+            fill(255, 0, 0);
+            percent = floor(object[i].confidence * 100);
+            text(object[i].label + " " + percent + " %", object[i].x + 15, object[i].y + 15);
+            noFill();
+            stroke("red");
+            rect(object[i].x, object[i].y, object[i].width, object[i].height);
+
+            if (object.label == "person"){
+                sound.stop();
+                document.getElementById("detected").innerHTML = "Baby Found";
+            }else{
+                sound.play();
+                document.getElementById("detected").innerHTML = "Baby Not Found";
+            }
+            
+        }
+
+        if(object.length < 0){
+                sound.play();
+                document.getElementById("detected").innerHTML = "Baby Not Found";
+        }
+    }
+}
